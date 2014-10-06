f269f3e69200747727a8ed5d336bce6e0002d0d4
Bode Fuwa <bode.fuwa@opusgroupllc.com>
2014-10-06 16:06:51 -0400

    Added reports controller

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,59 @@
+class ReportsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def generate_employees
+
+    end
+
+    def generate_departments(department)
+
+    end
+
+    def generate_invoices
+
+    end
+
+    def generate_vendors
+
+    end
+
+    def generate_cases
+
+    end
+
+    def generate_tickets
+
+    end
+
+    def report_params
+
+    end
+end
